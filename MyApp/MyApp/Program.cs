f4521e4d283675680cbe3b2fa821a68502e7f452
reachthefinals <s27370@pjwstk.edu.pl>
2024-03-12 11:29:49 +0100

    GetAverage

--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+static double GetAverage(int[] numbers)
+{
+    int sum = 0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+
+    return (double)sum / numbers.Length;
+}
